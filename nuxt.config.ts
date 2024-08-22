30b777714efbc36b043cc572ba5c912d6071bb12
AgeychevDenis <ageychevdenis@gmail.com>
2024-08-23 01:02:25 +0300

    🔧 chore: change nuxt config - vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,9 +29,9 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBase: process.env.API_URL,
-      origin: process.env.NUXT_PUBLIC_ORIGIN || 'https://nuxt-pizza.vercel.app/'
+      origin: process.env.NUXT_PUBLIC_ORIGIN || 'https://nuxt-pizza.vercel.app/',
     },
-    authOrigin: 'https://nuxt-pizza.vercel.app/'
+    authOrigin: 'https://nuxt-pizza.vercel.app/',
   },
   auth: {
     provider: {
@@ -39,6 +39,14 @@ export default defineNuxtConfig({
       trustHost: false,
       defaultProvider: 'github',
       addDefaultCallbackUrl: true,
+    },
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ['@prisma/client'],
+    },
+    ssr: {
+      noExternal: ['@prisma/client'],
     },
   },
 })
